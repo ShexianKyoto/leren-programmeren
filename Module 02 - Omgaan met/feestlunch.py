f2c73078cce1d9939478fbaed19e47276c4dc37b
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-08 15:06:22 +0200

    Module 02 - feestlunch.py & speelhal.py updated

--- Module 02 - Omgaan met/feestlunch.py
+++ Module 02 - Omgaan met/feestlunch.py
@@ -1,18 +1,19 @@
-aantalcroissant = int(17)
-croissantsp = float(0.39)
-totaalcroissant = aantalcroissant * croissantsp
-aantalstokbrood = int(2)
-stokbroodsp = float(2.78)
-totaalstokbrood = aantalstokbrood * stokbroodsp
-aantalkortingsbon = int(3)
-kortingswaarde = float(0.50)
-korting = aantalkortingsbon * kortingswaarde
+aantalcroissant = input('Voer het aantal croissantjes in: ')
+aantalcroissant1 = int(aantalcroissant)
+croissantsp = 39 #cent
+totaalcroissant = aantalcroissant1 * croissantsp
+
+aantalstokbrood = input('Voer het aantal stokbroden in: ')
+aantalstokbrood1 = int(aantalstokbrood)
+stokbroodsp = 278 #cent
+totaalstokbrood = aantalstokbrood1 * stokbroodsp
+
+aantalkortingsbon = input('Voer het aantal kortingsbonnen in: ')
+aantalkortingsbon1 = int(aantalkortingsbon)
+kortingswaarde = 50 #cent
+
+korting = aantalkortingsbon1 * kortingswaarde
 totaalprijs = totaalcroissant + totaalstokbrood - korting
 
-print('Prijs Croissantjes: ', totaalcroissant)
-print('Prijs Stokbroden: ', totaalstokbrood)
-print('Korting: ', aantalkortingsbon * kortingswaarde)
-print('Totaalprijs: ', totaalprijs)
-
-print(f'De feestlunch kost je bij de bakker {totaalprijs} euro voor de {aantalcroissant} croissantjes en de {aantalstokbrood} \
-stokbroden als de {aantalkortingsbon} kortingsbonnen nog geldig zijn!')
+print(f'De feestlunch kost je bij de bakker {totaalprijs} eurocent voor de {aantalcroissant1} croissantjes en de {aantalstokbrood1} \
+stokbroden als de {aantalkortingsbon1} kortingsbonnen nog geldig zijn!')
--- Module 02 - Omgaan met/speelhal.py
+++ Module 02 - Omgaan met/speelhal.py
@@ -1,15 +1,11 @@
-personen = int(4)
-ticketprijs = float(7.45)
+personen = int(input('Voer het aantal personen in: '))
+ticketprijs = 745 #cent
 entreekosten = personen * ticketprijs
-gameseatprijs = float(0.37) #per 5 minuten
-gameseatduur = int(5) 
-gameseatprijsduur = int(45) #minuten
+gameseatprijs = 37 #cent per 5 minuten
+gameseatduur = 5 #minuten
+gameseatprijsduur = int(input('Voer uw gameseat duur in in minutes: '))
 gameseat = gameseatprijsduur / gameseatduur * gameseatprijs
 totaalprijs = entreekosten + gameseat 
 
-print('Prijs entree: ', entreekosten)
-print('Prijs gameseat: ', gameseat)
-print('Totaalprijs: ', totaalprijs)
-
 print(f'Dit geweldige dagje-uit met {personen} mensen in de Speelhal met {gameseatprijsduur} minuten VR kost je maar \
-{totaalprijs} euro.')
+{totaalprijs} eurocent.')
