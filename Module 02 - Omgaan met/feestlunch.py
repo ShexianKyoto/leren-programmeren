3771d1f4bfbd36e6d28c94b6e5e2c3321f214934
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-08 14:06:19 +0200

    Module 02 - feestlunch.py & speelhal.py updated

--- Module 02 - Omgaan met/feestlunch.py
+++ Module 02 - Omgaan met/feestlunch.py
@@ -7,8 +7,12 @@ totaalstokbrood = aantalstokbrood * stokbroodsp
 aantalkortingsbon = int(3)
 kortingswaarde = float(0.50)
 korting = aantalkortingsbon * kortingswaarde
+totaalprijs = totaalcroissant + totaalstokbrood - korting
 
 print('Prijs Croissantjes: ', totaalcroissant)
 print('Prijs Stokbroden: ', totaalstokbrood)
 print('Korting: ', aantalkortingsbon * kortingswaarde)
-print('Totaalprijs: ', totaalcroissant + totaalstokbrood - korting)
+print('Totaalprijs: ', totaalprijs)
+
+print(f'De feestlunch kost je bij de bakker {totaalprijs} euro voor de {aantalcroissant} croissantjes en de {aantalstokbrood} \
+stokbroden als de {aantalkortingsbon} kortingsbonnen nog geldig zijn!')
--- Module 02 - Omgaan met/speelhal.py
+++ Module 02 - Omgaan met/speelhal.py
@@ -5,7 +5,11 @@ gameseatprijs = float(0.37) #per 5 minuten
 gameseatduur = int(5) 
 gameseatprijsduur = int(45) #minuten
 gameseat = gameseatprijsduur / gameseatduur * gameseatprijs
+totaalprijs = entreekosten + gameseat 
 
 print('Prijs entree: ', entreekosten)
 print('Prijs gameseat: ', gameseat)
-print('Totaalprijs: ', entreekosten + gameseat)
+print('Totaalprijs: ', totaalprijs)
+
+print(f'Dit geweldige dagje-uit met {personen} mensen in de Speelhal met {gameseatprijsduur} minuten VR kost je maar \
+{totaalprijs} euro.')
