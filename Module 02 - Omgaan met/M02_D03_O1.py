c9db5860d55a67fc9a3d491ab5c35f80124f47cb
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-09 09:42:13 +0200

    Module 02 - input en if-statement

---
+++ Module 02 - Omgaan met/M02_D03_O1.py
@@ -0,0 +1,8 @@
+a = int(input('Voer een geheel getal in: '))
+b = int(input('Voer nog een geheel getal in: '))
+
+if a > b:
+    Max = a
+    print('a is het grootste getal', Max)
+
+#input en if-statement
