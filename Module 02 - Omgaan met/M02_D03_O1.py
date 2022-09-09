e6e9ff0f3084530af0d7499546f7328b7fc4470f
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-09 09:57:55 +0200

    Deel 03 - Min en Max

--- Module 02 - Omgaan met/M02_D03_O1.py
+++ Module 02 - Omgaan met/M02_D03_O1.py
@@ -3,11 +3,15 @@ b = int(input('Voer nog een geheel getal in: '))
 
 if a > b:   #input en if-statement
     Max = a
-    print('a is het grootste getal', Max)
+    Min = b
+    print('Het minimum is: ', Min)
+    print('Het maximum is: ', Max)
 
 elif a < b:     #elif-statement
     Min = a
-    print('a is het kleinste getal', Min)
-
+    Max = b
+    print('Het minimum is: ', Min)
+    print('Het maximum is: ', Max)
+    
 else:       #else-statement
     print('a en b zijn even groot')
