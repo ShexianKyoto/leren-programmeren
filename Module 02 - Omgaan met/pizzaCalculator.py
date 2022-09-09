f0df45f29d6b231d4cf0a21ab4754ec038e13ef8
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-09 09:33:09 +0200

    Module 02 - pizzaCalculator.py updated

--- Module 02 - Omgaan met/pizzaCalculator.py
+++ Module 02 - Omgaan met/pizzaCalculator.py
@@ -1,10 +1,10 @@
 #Rayen ter Wal | Pizza Calculator
 print("Welkom!")
-afmeting = input('Kies uw afmeting pizza (Small, Medium of Large): ')
+afmeting = input('Kies uw afmeting pizza (Small, Medium of Large): ').lower()
 print('-' * 30)
 
 if afmeting == 'small':
-    aantalsmall = int(input("Voer uw aantal Small pizza's in: "))
+    aantalsmall = int(input("Voer uw aantal pizza's in: "))
     print('-' * 30)
     prijssmall = 7.49
     kostensmall = aantalsmall * prijssmall
@@ -13,7 +13,7 @@ if afmeting == 'small':
     print("Bedankt voor het bestellen!")
 
 elif afmeting == 'medium':
-    aantalmedium = int(input("Voer uw aantal Medium pizza's in: "))
+    aantalmedium = int(input("Voer uw aantal pizza's in: "))
     print('-' * 30)
     prijsmedium = 8.99
     kostenmedium = aantalmedium * prijsmedium
@@ -22,7 +22,7 @@ elif afmeting == 'medium':
     print("Bedankt voor het bestellen!")
 
 elif afmeting == 'large':
-    aantallarge = int(input("Voer uw aantal Large pizza's in: "))
+    aantallarge = int(input("Voer uw aantal pizza's in: "))
     print('-' * 30)
     prijslarge = 12.49
     kostenlarge = aantallarge * prijslarge
@@ -31,4 +31,4 @@ elif afmeting == 'large':
     print("Bedankt voor het bestellen!")
 
 else:
-    print('Ik heb uw antwoord niet begrepen, probeer het opnieuw (zonder hoofdletters).')
+    print('Ik heb uw antwoord niet begrepen, probeer het opnieuw.')
