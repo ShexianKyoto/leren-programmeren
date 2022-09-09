d61a19a2b6038a427d552f1326727b1256597cdb
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-09 09:44:45 +0200

    Add files via upload

--- Module 02 - Omgaan met/M02_D03_O1.py
+++ Module 02 - Omgaan met/M02_D03_O1.py
@@ -1,8 +1,10 @@
 a = int(input('Voer een geheel getal in: '))
 b = int(input('Voer nog een geheel getal in: '))
 
-if a > b:
+if a > b:   #input en if-statement
     Max = a
     print('a is het grootste getal', Max)
 
-#input en if-statement
+elif a < b:
+    Min = a
+    print('a is het kleinste getal', Min)
