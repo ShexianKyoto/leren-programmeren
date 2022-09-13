e2bc5b93c35df825964ba0e36c3033e5f8d0dbcd
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-13 11:52:36 +0200

    M02.D03.O3 - sollicitatie.py updated

--- Module 02 - Omgaan met/sollicitatie.py
+++ Module 02 - Omgaan met/sollicitatie.py
@@ -1,9 +1,10 @@
 print('Om te solliciteren voor de vacature van \'Circusdirecteur voor Circus HotelDeBoer\' moet u voldoen aan bepaalde eisen,\
     hiervoor zullen wij u een paar vragen stellen. Als het blijkt dat u voldoet aan de benodigde criteria heeft u kans op\
     een uitnodiging van een sollicitatiegesprek.')
-ervaring_dierendressuur = input('Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?: ').lower()
-ervaring_jongleren = input('Hoeveel jaar ervaring heeft u met jongleren?: ').lower()
-ervaring_acrobatiek = input('Hoeveel jaar praktijkervaring heeft u met acrobatiek?: ').lower()
+
+ervaring_dierendressuur = int(input('Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?: '))
+ervaring_jongleren = int(input('Hoeveel jaar ervaring heeft u met jongleren?: '))
+ervaring_acrobatiek = int(input('Hoeveel jaar praktijkervaring heeft u met acrobatiek?: '))
 bezit_diplomaMBO4 = input('Bent u in bezit van het Diploma MBO-4 ondernemen?: ').lower()
 bezit_geldigVrachtwagenRijbewijs = input('Bent u in bezit van een geldig vrachtwagenrijbewijs?: ').lower()
 bezit_hogeHoed = input('Bent u in bezit van een hoge hoed?: ').lower()
@@ -25,16 +26,15 @@ heeft_conditie = input('Beschikt u over een goede conditie i.v.m sportdag?: ').l
 heeft_partner = input('Heeft u een partner?: ').lower()
 
 toelating = False
-if ervaring_dierendressuur >= 4: toelating = True
-if ervaring_jongleren >= 5: toelating = True
-if ervaring_acrobatiek >= 3: toelating = True
+if ervaring_dierendressuur >= 4 or ervaring_jongleren >= 5 or ervaring_acrobatiek >= 3: toelating = True
 if bezit_diplomaMBO4 == 'ja': toelating = True
 if bezit_geldigVrachtwagenRijbewijs == 'ja': toelating = True
 if bezit_hogeHoed == 'ja': toelating = True
-if geslacht == 'man' & heeft_snor == 'ja' & snorlengte >= 10: toelating = True
-if geslacht == 'vrouw' & heeft_rood_krulhaar == 'ja' & lengte_krulhaar >= 20: toelating = True
+if geslacht == 'man' and heeft_snor == 'ja' and snorlengte >= 10: toelating = True
+if geslacht == 'vrouw' and heeft_rood_krulhaar == 'ja' and lengte_krulhaar >= 20: toelating = True
 if lichaamslengte >= 150: toelating = True
 if lichaamsgewicht >= 90: toelating = True
 if certificaat_overlevenMetGevaarlijkPersoneel == 'ja': toelating = True
+
 if toelating == True: print('Gefeliciteerd! U maakt kans op een sollicitatiegesprek.')
-else: print('Helaas! U maakt geen kans op een sollicitatiegesprek.')
+elif toelating == False: print('Helaas! U maakt geen kans op een sollicitatiegesprek.')
