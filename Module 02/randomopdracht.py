eb1f6787e2bffd6a6e9fab42d4104c3a8a8338e3
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-09-15 17:36:50 +0200

    Delete randomopdracht.py

--- Module 02/randomopdracht.py
+++
@@ -1,9 +0,0 @@
-#leeftijd = input('Wat is jouw leeftijd?: ')
-#nieuwe_leeftijd = int(leeftijd) * 3
-#print('Als je 3x zo oud zou zijn, was je {} jaar oud geweest!'.format(nieuwe_leeftijd))
-
-tijd_uren = int(input('Hoelaat is het en wil je alleen de uren benoemen (08)?: '))
-tijd_minuten = int(input('Bedankt! En wil je nu alleen de minuten benoemen (30)?: '))
-uren = 23 - tijd_uren
-minuten = 60 - tijd_minuten
-print('Dan duurt het nog {} uur en {} minuten voordat de dag voorbij is.'.format(uren, minuten))
