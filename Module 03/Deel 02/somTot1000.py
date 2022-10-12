21afbd3c7a5e40d3b7b1abfe8117b370788dc350
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-10-12 11:20:34 +0200

    somTot1000.py added

---
+++ Module 03/Deel 02/somTot1000.py
@@ -0,0 +1,10 @@
+addition = 51
+equasion = 50
+iterate = 1
+toPrint = "50",
+while equasion < 1000:
+    equasion += addition
+    toPrint += "+", addition
+    print(f'{iterate}. {toPrint} = {equasion}')
+    iterate += 1
+    addition += 1
