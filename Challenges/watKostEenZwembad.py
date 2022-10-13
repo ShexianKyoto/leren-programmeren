51dd75a6238f04b1db3724b8f1cf9f6b680295f0
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-10-13 09:25:47 +0200

    watKostEenZwembad.py added

---
+++ Challenges/watKostEenZwembad.py
@@ -0,0 +1,8 @@
+# Voorrijkosten (afhankelijk van de afstand en grootte van het zwembad)
+#   inhoud zwembad mag afronden 2 decimalen, joris kijkt niet naar kruiwagen grond meer of minder.
+# Kosten uitgraven (gerekend in m3(kubieke))
+# Kosten afvoeren grond (gerekend per m3)
+# Kosten aanbrengen wand en vloer (gerekend per m2(vierkante))
+# Test uitzondering, commit en push na elke stap!
+
+print((8*3)*1.5)
