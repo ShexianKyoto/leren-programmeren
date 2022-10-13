7b2caa2d827e8fb35563cae05ec879a98c104f91
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-10-13 09:50:44 +0200

    updated

--- Challenges/watKostEenZwembad.py
+++ Challenges/watKostEenZwembad.py
@@ -4,14 +4,19 @@
 # Kosten afvoeren grond (gerekend per m3)
 # Kosten aanbrengen wand en vloer (gerekend per m2(vierkante))
 # Test uitzondering, commit en push na elke stap!
-
-hoeveelGrond = (8*3)*1.5
+hoogte = float(input('Voer de de hoogte van het zwembad in (in meters(vb. 13.25)): '))
+breedte = float(input('Voer de de breedte van het zwembad in (in meters(vb. 7.49)): '))
+diepte = float(input('Voer de de diepte van het zwembad in (in meters(vb. 2.13)): '))
+hoeveelGrond = (hoogte*breedte)*diepte
 print(hoeveelGrond)
 
-kostenUitgraven = hoeveelGrond*25 #per m3
-kostenAfvoeren = hoeveelGrond*32.50 #per m3
+kostenUitgraven = hoeveelGrond*25 #vaste prijs per m3
+kostenAfvoeren = hoeveelGrond*32.50 #vaste prijs per m3
+kostenVoorrij = 250+(60*2.05)
+
 print(f'''Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud: {hoeveelGrond} m3)
 Uitgraven      :  €{kostenUitgraven}
 Afvoeren Grond :  €{kostenAfvoeren}
-Totaal         :  €{kostenUitgraven+kostenAfvoeren}
+Voorrijkosten  :  €{kostenVoorrij}
+Totaal         :  €{kostenUitgraven+kostenAfvoeren+kostenVoorrij}
 ''')
