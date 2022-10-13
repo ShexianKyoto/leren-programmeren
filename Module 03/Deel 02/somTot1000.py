c69931050cf11b6e3f17bc95ab110720732c479c
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-10-13 09:30:31 +0200

    updated

--- Module 03/Deel 02/somTot1000.py
+++ Module 03/Deel 02/somTot1000.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 addition = 51
 equasion = 50
 iterate = 1
@@ -8,15 +7,4 @@ while equasion <= 1000:
     toPrint += "+", addition
     print(f'{iterate}. {toPrint} = {equasion}')
     iterate += 1
-=======
-addition = 51
-equasion = 50
-iterate = 1
-toPrint = "50",
-while equasion < 1000:
-    equasion += addition
-    toPrint += "+", addition
-    print(f'{iterate}. {toPrint} = {equasion}')
-    iterate += 1
->>>>>>> 21afbd3c7a5e40d3b7b1abfe8117b370788dc350
-    addition += 1
+        addition += 1
