b9a596198147a6832a2de55f08451e31b5ded76f
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-10-13 09:37:13 +0200

    updated zwembad

--- Challenges/watKostEenZwembad.py
+++ Challenges/watKostEenZwembad.py
@@ -5,4 +5,13 @@
 # Kosten aanbrengen wand en vloer (gerekend per m2(vierkante))
 # Test uitzondering, commit en push na elke stap!
 
-print((8*3)*1.5)
+hoeveelGrond = (8*3)*1.5
+print(hoeveelGrond)
+
+kostenUitgraven = hoeveelGrond*25 #per m3
+kostenAfvoeren = hoeveelGrond*32.50 #per m3
+print(f'''Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud: {hoeveelGrond} m3)
+Uitgraven      :  €{kostenUitgraven}
+Afvoeren Grond :  €{kostenAfvoeren}
+Totaal         :  €{kostenUitgraven+kostenAfvoeren}
+''')
