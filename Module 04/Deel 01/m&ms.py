f88f315c44365e0db98dc1be2a13c4051a586d10
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-11-17 10:17:01 +0100

    updated

---
+++ Module 04/Deel 01/m&ms.py
@@ -0,0 +1,10 @@
+import random
+
+kleuren = ('oranje', 'blauw', 'groen', 'bruin')
+hoeveelheid = int(input('Hoeveel M&M\'s wil je toevoegen: '))
+zak = []
+
+for willekeurig in range(1, hoeveelheid+1):
+    randomkleur = random.choice(kleuren)
+    zak.append(randomkleur)
+print(zak)
