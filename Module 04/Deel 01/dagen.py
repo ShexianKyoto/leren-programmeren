acceeac4cee5deb96b90a2a8c57fa6c2da0e6ffc
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-11-22 11:18:40 +0100

    dagen.py erg verkort

--- Module 04/Deel 01/dagen.py
+++ Module 04/Deel 01/dagen.py
@@ -1,19 +1,9 @@
-werkdagen = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag')
-weekenddagen = ('zaterdag', 'zondag')
-weekdagen = werkdagen + weekenddagen
-
-print(f'Alle dagen van de week zijn: {weekdagen}')
-print(f'De werkdagen zijn: {werkdagen}')
-print(f'De weekenddagen zijn: {weekenddagen}')
-
-print(f'Alle dagen van de week in omgekeerde volgorde zijn:')
-for i in range(1, 8):
-    print(f'{weekdagen[-i]}')
-print(f'De werkdagen in omgekeerde volgorde zijn:')
-for i in range(1, 6):
-    print(f'{werkdagen[-i]}')
-print(f'De weekenddagen in omgekeerde volgorde zijn:')
-for i in range(1, 3):
-    print(f'{weekenddagen[-i]}')
-
-# verander lijst naar tuple en in 1 tuple
+week = ('maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag')
+print(f'''
+Alle dagen van de week zijn: {week}
+De werkdagen zijn: {week[0:5]}
+De weekenddagen zijn: {week[5:7]}
+Alle dagen van de week in omgekeerde volgorde zijn: {week[::-1]}
+De werkdagen in omgekeerde volgorde zijn: {week[-3::-1]}
+De weekenddagen in omgekeerde volgorde zijn: {week[-1:-3:-1]}
+''')
