93deeb8723bca0dc6fde1b73aa1e99fdbc3f41c0
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-11-22 09:23:23 +0100

    m&ms2 verbetered naar 12 lines ipv 36

--- Module 04/Deel 01/m&ms2.py
+++ Module 04/Deel 01/m&ms2.py
@@ -3,34 +3,10 @@ import random
 kleuren = ('rood', 'blauw', 'groen', 'geel', 'bruin')
 hoeveelheid = int(input('''Hoeveel M&M\'s wil je toevoegen aan de zak?
 '''))
-zak = {
-    # 'rood': 0,
-    # 'blauw': 0,
-    # 'groen': 0,
-    # 'geel': 0,
-    # 'bruin': 0,
-}
+zak = {}
 
-for willekeurig in range(1, hoeveelheid+1):
+for randomk in range(hoeveelheid):
     randomkleur = random.choice(kleuren)
-    if randomkleur == 'rood': 
-        if 'rood' not in zak.keys():
-            zak.update({'rood': 0})
-        else: zak['rood'] += 1
-    if randomkleur == 'blauw':
-        if 'blauw' not in zak.keys():
-            zak.update({'blauw': 0})
-        else: zak['blauw'] += 1
-    if randomkleur == 'groen': 
-        if 'groen' not in zak.keys():
-            zak.update({'groen': 0})
-        else: zak['groen'] += 1
-    if randomkleur == 'geel':
-        if 'geel' not in zak.keys():
-            zak.update({'geel': 0})
-        else: zak['geel'] += 1
-    if randomkleur == 'bruin':
-        if 'bruin' not in zak.keys():
-            zak.update({'bruin': 0})
-        else: zak['bruin'] += 1
+    if randomkleur not in zak: zak.update({randomkleur: 1})
+    else: zak[randomkleur] += 1
 print(f'Zak: {zak}')
--- SoftSkills/startTheParty.py
+++
@@ -1,9 +0,0 @@
-gastheer=input('Voer de naam van de gastheer in: ').lower()
-gasten=13
-drank=True
-chips=True
-
-if drank and gastheer != 'rudi' and gasten >= 5 and (gastheer != '' and gasten+1 <= 13):
-    print('Start the party')
-else:
-    print('No party')
