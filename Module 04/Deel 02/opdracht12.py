36d62c10e2a7df6b1d051fb7eb49d72fadd83475
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-11-29 12:10:56 +0100

    opdracht 12 gefixed

--- Module 04/Deel 02/opdracht12.py
+++ Module 04/Deel 02/opdracht12.py
@@ -34,14 +34,18 @@ fruitmand = [{
     'color' : 'yellow',
     'round' : True
 }]
-for i in fruitmand:
-    naam = i['name']
-    lengte = len(i['name'])
-    kleur = i['color']
-    if kleur == 'yellow': kleur = 'gele' #geel groen oranje rood bruin
-    elif kleur == 'green': kleur = 'groene'
-    elif kleur == 'orange': kleur = 'oranje'
-    elif kleur == 'red': kleur = 'rode'
-    elif kleur == 'brown': kleur = 'bruine'
-    gewicht = i['weight'] / 1000
-    print(f'De "{naam}" ({lengte} letters) heeft een {kleur} kleur en een gewicht van {gewicht} kg.')
+def sorteer(i):
+    return len(i['name'])
+fruitmand.sort(key=sorteer)
+
+naam = fruitmand[-1]['name']
+lengte = len(fruitmand[-1]['name'])
+kleur = fruitmand[-1]['color']
+if kleur == 'yellow': kleur = 'gele'
+elif kleur == 'green': kleur = 'groene'
+elif kleur == 'orange': kleur = 'oranje'
+elif kleur == 'red': kleur = 'rode'
+elif kleur == 'brown': kleur = 'bruine'
+gewicht = fruitmand[-1]['weight'] / 1000
+
+print(f'De "{naam}" ({lengte} letters) heeft een {kleur} kleur en een gewicht van {gewicht} kg.')
