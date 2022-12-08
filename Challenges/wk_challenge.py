681409a083bb791b737645d1506ecbf333be8351
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2022-12-08 09:21:24 +0100

    vraag 1

---
+++ Challenges/wk_challenge.py
@@ -0,0 +1,2 @@
+# 32 landen, 8 groepen, per groep 1 land tegen ieder ander land, per groep 6 wedstrijden, vervolgens afvalrace
+groepA1, groepA2, groepA3 = input('Welke landen spelen in Groep A? (Alles op 1 zin):  ').split()
