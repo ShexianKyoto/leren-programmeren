8467eeb3d71a8fd562cd88c3745a72e6ebf3bba1
ShexianKyoto <38539072+ShexianKyoto@users.noreply.github.com>
2023-01-10 10:05:40 +0100

    fixed

--- Module 04/Deel 02/opdracht12.py
+++ Module 04/Deel 02/opdracht12.py
@@ -6,9 +6,7 @@ fruitmand.sort(key=sorteer)
 
 naam = fruitmand[-1]['name']
 lengte = len(fruitmand[-1]['name'])
-kleur = fruitmand[-1]['color']
-if kleur == 'orange': kleur = 'oranje'
-elif kleur == 'pink': kleur = 'roze'
+kleur = fruitmand[-1]['color'] = 'oranje'
 gewicht = fruitmand[-1]['weight'] / 1000
 
 print(f'De "{naam}" ({lengte} letters) heeft een {kleur} kleur en een gewicht van {gewicht} kg.')
